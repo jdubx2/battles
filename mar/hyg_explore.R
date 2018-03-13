47fb7c50b9037208772e5ea01b0f8bafb568ba82
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-03-12 22:45:32 -0700

    add detail bars

--- mar/hyg_explore.R
+++ mar/hyg_explore.R
@@ -42,9 +42,9 @@ df3 %>%
             avg_absmag = mean(absmag),
             avg_lum = mean(lum))
   
-  df3 %>% 
+  p0 <- df3 %>% 
     mutate(lum = ifelse(lum > 5000,5000,lum),
-           sz = ifelse(lum > 1500, 1500,lum)) %>% 
+           sz = ifelse(lum > 1700, 1700,lum)) %>% 
     mutate(spect2 = ifelse(spect1 %in% c('O','B'), 'O/B', spect1),
            spect2 = paste0('Class ', spect2),
            spect2 = factor(spect2, 
@@ -52,24 +52,93 @@ df3 %>%
     ggplot(aes(x=x2, y = y2, color = hex)) + 
       geom_point(aes(alpha = lum, size = sz)) +
       scale_color_manual(values = col) +
-      scale_size_continuous(range = c(.005,1.03))+
-      scale_alpha_continuous(range = c(.085,1)) +
+      scale_size_continuous(range = c(.005,.99))+
+      scale_alpha_continuous(range = c(.08,1)) +
       coord_flip()+
       guides(color = F) +
       theme(plot.background = element_rect(fill = 'gray3', color ='gray3'),
             panel.background = element_rect(fill = 'gray3', color ='gray3'),
             panel.grid = element_blank(),
             strip.background = element_rect(color = 'gray3', fill = 'gray3'),
-            strip.text = element_text(color = 'gray90', family = 'Consolas', size = 23),
+            strip.text = element_text(color = 'gray90', family = 'Consolas', size = 19),
             axis.text = element_blank(),
             axis.ticks = element_blank(),
             axis.title = element_blank(),
-            plot.title = element_text(size = 30, family = 'Consolas', color = 'gray90', hjust = .5),
-            plot.subtitle = element_text(size = 20, family = 'Consolas', color = 'gray90', hjust = .5)) +
-      facet_wrap(~spect2) +
+            plot.title = element_text(size = 28, family = 'Consolas', color = 'gray90', hjust = .5),
+            plot.subtitle = element_text(size = 19, family = 'Consolas', color = 'gray90', hjust = .5),
+            plot.margin = margin(1,1,4.5,1,unit = 'cm')) +
+      facet_wrap(~spect2, nrow=1) +
     guides(size = F, alpha = F)+
-    labs(title = 'Stars by Spectral Class', subtitle = '100k+ Stars from the HYG Database')
+    labs(title = 'Spectral Classification of Stars')
   
+  ggsave(file="stars.png", plot=p0, width=14.8, height=5.3, bg = 'transparent')
+  
+temps_df <- data.frame(class = factor(c('O', 'B', 'A', 'F', 'G', 'K', 'M'), ordered = T),
+                       temp = rev(c(3500,5000,6000,7500,11000,25000,40000)),
+                       share = c(.0003,.13, .6,3,7.6, 12.1,76.45),
+                       hex = c("#bdf0ff", "#ddf2ff","#fff0ff","#ffebd1","#ffe49b","#ffcf49","#ffbe28"))
+  
+col2 <- as.character(temps_df$hex)
+names(col2) <- as.character(temps_df$hex)
+
+
+
+library(cowplot)
+
+p1 <- temps_df %>% 
+  ggplot(aes(x = reorder(class, temp), y = temp, fill = hex))+
+  geom_col(alpha = .9)+
+  scale_fill_manual(values = col2)+
+  theme(plot.background = element_rect(fill = 'gray3', color ='gray3'),
+        panel.background = element_rect(fill = 'gray3', color ='gray3'),
+        panel.grid = element_blank(),
+        strip.background = element_rect(color = 'gray3', fill = 'gray3'),
+        strip.text = element_text(color = 'gray90', family = 'Consolas', size = 19),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())+
+  guides(fill = F)+
+  coord_flip()+
+  scale_y_reverse(expand = c(0,0))
+
+p2 <- temps_df %>% 
+  ggplot(aes(x = reorder(class, -share), y = share, fill = hex))+
+  geom_col(alpha = .9)+
+  scale_fill_manual(values = col2)+
+  theme(plot.background = element_rect(fill = 'gray3', color ='gray3'),
+        panel.background = element_rect(fill = 'gray3', color ='gray3'),
+        panel.grid = element_blank(),
+        strip.background = element_rect(color = 'gray3', fill = 'gray3'),
+        strip.text = element_text(color = 'gray90', family = 'Consolas', size = 19),
+        axis.text.y = element_text(color = 'gray85', family = 'Consolas', size = 21),
+        axis.text.x = element_blank(),
+        axis.ticks.y = element_line(color= 'white'),
+        axis.title = element_blank())+
+  guides(fill = F)+
+  coord_flip()+
+  scale_y_continuous(expand = c(0,0))
+
+
+p4 <- plot_grid(p1,p2)
+
+ggsave(file="bars.svg", plot=p4, width=18.6, height=1.9, bg = 'transparent')
+
+
+grad %>% 
+  ggplot(aes(x = bv, y = 1, fill = hex))+
+  geom_col()+
+  scale_fill_manual(values = col) +
+  guides(fill = F) +
+  theme(plot.background = element_rect(fill = 'gray3', color ='gray3'),
+        panel.background = element_rect(fill = 'gray3', color ='gray3'),
+        panel.grid = element_blank(),
+        strip.background = element_rect(color = 'gray3', fill = 'gray3'),
+        strip.text = element_text(color = 'gray90', family = 'Consolas', size = 19),
+        axis.text = element_text(color='white'),
+        axis.ticks = element_line(color= 'white'),
+        axis.title = element_blank())+
+  scale_x_continuous(breaks = seq(-1,2,.2))+
+  scale_y_continuous(expand =c(0,0))
   
   
   df4 <- df3 %>% filter(con =='Psc') %>% 
